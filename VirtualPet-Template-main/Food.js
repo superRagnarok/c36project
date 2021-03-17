2e2cbac1bde5146433929327d4e1403a6fb75201
superRagnarok <73694075+superRagnarok@users.noreply.github.com>
2021-03-17 21:36:56 +0530

    Add files via upload

---
+++ VirtualPet-Template-main/Food.js
@@ -0,0 +1,41 @@
+class Food {
+    constructor(){
+      this.image = loadImage("Images/Milk.png");
+      this.foodStock=5;
+      this.lastFed;
+    }
+    display(){
+var x=80, y=100;
+
+imageMode(CENTER);
+image(this.image,720,220,70,70);
+
+if (this.foodStock !== 0){
+    console.log(this.foodStock);
+    for(var i=0; i<this.foodStock; i++){
+        if (i%10 == 0){
+            x=80;
+            y=y+50;        
+        }
+        image(this.image,x,y,50,50);
+        x=x+30;
+    }
+    
+    
+}
+    }
+    getFoodStock(){
+        
+        return this.foodStock;
+
+    }
+    updateFoodStock(foodStock){
+ this.foodStock = foodStock;
+ return this.foodStock;
+    }
+    deductFood(foodStock){
+  if (this.foodStock>0){
+      this.foodStock = foodStock -1;
+  }
+    }
+}
